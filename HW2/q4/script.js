5e9822921c6b377235936effe2850bbcbe204f5b
= <=>
2025-11-02 17:39:11 +0330

    shuffling and restart

--- HW2/q4/script.js
+++ HW2/q4/script.js
@@ -1,119 +1,156 @@
 const cards = [
     {
         Id: 0,
-        Content: 0,
-        show: false 
+        Content: "A",
+        show: false
     },
     {
         Id: 1,
-        Content: 1,
-        show: false 
+        Content: "B",
+        show: false
     },
     {
         Id: 2,
-        Content: 2,
-        show: false 
+        Content: "C",
+        show: false
     },
     {
         Id: 3,
-        Content: 3,
-        show: false 
+        Content: "D",
+        show: false
     },
     {
         Id: 4,
-        Content: 0,
-        show: false 
+        Content: "E",
+        show: false
     },
     {
         Id: 5,
-        Content: 0,
-        show: false 
+        Content: "F",
+        show: false
     },
     {
         Id: 6,
-        Content: 0,
-        show: false 
+        Content: "G",
+        show: false
     },
     {
         Id: 7,
-        Content: 0,
-        show: false 
+        Content: "H",
+        show: false
     },
     {
         Id: 8,
-        Content: 0,
-        show: false 
+        Content: "A",
+        show: false
     },
     {
         Id: 9,
-        Content: 0,
-        show: false 
+        Content: "B",
+        show: false
     },
 
     {
         Id: 10,
-        Content: 0,
-        show: false 
+        Content: "C",
+        show: false
     },
     {
         Id: 11,
-        Content: 0,
-        show: false 
+        Content: "D",
+        show: false
     },
     {
         Id: 12,
-        Content: 0,
-        show: false 
+        Content: "E",
+        show: false
     },
     {
         Id: 13,
-        Content: 0,
-        show: false 
+        Content: "F",
+        show: false
     },
     {
         Id: 14,
-        Content: 0,
+        Content: "G",
         show: false
     },
     {
         Id: 15,
-        Content: 0,
+        Content: "H",
         show: false
     },
 ]
 
-function show(cardId){
-    cards.find(card => card.Id === cardId).show =true
+function restart(){
+    hideAll();
+    randomize();
 }
-function hide(cardId){
+
+function hideAll(){
+    for(let card of cards){
+        card.show = false;
+    }
+}
+
+function randomize() {
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
+    const shuffled = shuffle(letters);
+    for(let i in cards){
+        cards[i].Content = shuffled[i];
+    }
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+function show(cardId) {
+    cards.find(card => card.Id === cardId).show = true
+}
+function hide(cardId) {
     cards.find(card => card.Id === cardId).show = false
 }
 
-function updateUI(){
+function updateUI() {
     const board = document.getElementById("board")
     // for(const card of cards){
     //     board.querySelector("[]")
     // }
     const cardsUi = board.querySelectorAll(".card")
-    for(let cardUi of cardsUi){
+    for (let cardUi of cardsUi) {
         const cardId = Number(cardUi.dataset.id)
         const card = cards.find(card => card.Id === cardId)
-        if(!card.show){
+        if (!card.show) {
             cardUi.textContent = "";
             continue;
         }
         cardUi.textContent = card.Content
     }
 }
-function listenForCardClick(){
+function listenForCardClick() {
     const board = document.getElementById("board");
-    board.addEventListener("click",function(event){
+    board.addEventListener("click", function (event) {
         const clickedId = event.target.dataset.id;
         show(Number(clickedId));
         updateUI();
     });
 }
-document.addEventListener("DOMContentLoaded",function(event){
-    // updateUI()
+
+function listenForRestart(){
+    const restartButton = document.getElementById("restart");
+    restartButton.addEventListener("click",function(event){
+        restart();
+        updateUI();
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
     listenForCardClick();
+    listenForRestart();
+    randomize();
+    updateUI();
 });
