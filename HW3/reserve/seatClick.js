d0d6207c2e0a857a1985468209dad230e0775fcc
erfan <erfan>
2025-11-14 18:16:04 +0330

    make seat blue when touching

--- HW3/reserve/seatClick.js
+++ HW3/reserve/seatClick.js
@@ -9,24 +9,24 @@ async function setMoviePoster(movie) {
 
 async function setMovieDetails(id) {
     let selectedMovie = null;
-    for(let movie of movies.movies){
-        if(movie.id === id){
+    for (let movie of movies.movies) {
+        if (movie.id === id) {
             selectedMovie = movie;
         }
     }
-    setSimpleId("next-movie-text",selectedMovie.name);
+    setSimpleId("next-movie-text", selectedMovie.name);
     await setMoviePoster(selectedMovie);
 }
 
-async function updateUi(){
-    setSimpleId("next-hall-text",seats.selectedHall);
+async function updateUi() {
+    setSimpleId("next-hall-text", seats.selectedHall);
     const selectedHall = seats.hallsData.find(hall => hall.name === seats.selectedHall)
     const selectedMovieId = selectedHall.selectedMovieId;
     await setMovieDetails(selectedMovieId);
     const selectedMovie = getSelectedMovie();
     const seatsState = selectedMovie.seats;
     const seatsUis = document.getElementsByClassName("seat")
-    for(let seatUi of seatsUis){
+    for (let seatUi of seatsUis) {
         const col = seatUi.dataset.col;
         const row = seatUi.dataset.row;
         const key = row + col;
@@ -35,10 +35,11 @@ async function updateUi(){
     }
 }
 
-function setSimpleId(id,text){
+function setSimpleId(id, text) {
     const element = document.getElementById(id);
     element.innerText = text
 }
+
 async function initDataFromFile() {
     const rawSeatsData = await fetch('seats.json')
     seats = await rawSeatsData.json()
@@ -48,7 +49,7 @@ async function initDataFromFile() {
     console.log(movies)
 }
 
-document.addEventListener("DOMContentLoaded",async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     await initDataFromFile();
     updateUi();
 })
@@ -57,18 +58,19 @@ function nextHall() {
     const oldHall = seats.selectedHall;
     const oldHallIndex = seats.hallsData.findIndex(hall => hall.name === oldHall);
     let newHallIndex = oldHallIndex + 1;
-    if(newHallIndex >= seats.hallsData.length){
+    if (newHallIndex >= seats.hallsData.length) {
         newHallIndex = 0;
     }
     const newHall = seats.hallsData[newHallIndex];
     seats.selectedHall = newHall.name;
     updateUi();
 }
+
 function prevHall() {
     const oldHall = seats.selectedHall;
     const oldHallIndex = seats.hallsData.findIndex(hall => hall.name === oldHall);
     let newHallIndex = oldHallIndex - 1;
-    if(newHallIndex < 0){
+    if (newHallIndex < 0) {
         newHallIndex = seats.hallsData.length - 1;
     }
     const newHall = seats.hallsData[newHallIndex];
@@ -79,17 +81,19 @@ function prevHall() {
 function getSelectedHall() {
     return seats.hallsData.find(hall => hall.name === seats.selectedHall)
 }
-function getSelectedMovie(){
+
+function getSelectedMovie() {
     const selectedHall = getSelectedHall();
     const movieId = selectedHall.selectedMovieId;
     return selectedHall.movies.find(movie => movie.id === movieId);
 }
+
 function nextMovie() {
     const selectedHall = getSelectedHall();
     const oldMovieId = selectedHall.selectedMovieId;
     const oldMovieIndex = selectedHall.movies.findIndex(movie => movie.id === oldMovieId);
     let newMovieIndex = oldMovieIndex + 1;
-    if(newMovieIndex >= selectedHall.movies.length){
+    if (newMovieIndex >= selectedHall.movies.length) {
         newMovieIndex = 0;
     }
     console.log(`new movie index: ${newMovieIndex}`)
@@ -98,12 +102,13 @@ function nextMovie() {
     console.log(`new movie id: ${newMovieId}`)
     updateUi();
 }
+
 function prevMovie() {
     const selectedHall = getSelectedHall();
     const oldMovieId = selectedHall.selectedMovieId;
     const oldMovieIndex = selectedHall.movies.findIndex(movie => movie.id === oldMovieId);
     let newMovieIndex = oldMovieIndex - 1;
-    if(newMovieIndex < 0){
+    if (newMovieIndex < 0) {
         newMovieIndex = selectedHall.movies.length - 1;
     }
     console.log(`new movie index: ${newMovieIndex}`)
@@ -111,4 +116,15 @@ function prevMovie() {
     selectedHall.selectedMovieId = newMovieId;
     console.log(`new movie id: ${newMovieId}`)
     updateUi();
+}
+
+function seatClick(row, col, reserved) {
+    const seat = getSeat(row, col);
+    seat.reserved = !(JSON.parse(reserved));
+    updateUi();
+}
+
+function getSeat(row, col) {
+    const key = row + col;
+    return getSelectedMovie().seats.find(seat => seat.key === key);
 }
