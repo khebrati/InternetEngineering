6abc585defd81c31883c9623fafcf3badbd9f4d1
erfan <erfan>
2025-11-25 11:45:46 +0330

    add a try catch to handle encryption errors

--- HW4/handlers/handleEncryption.js
+++ HW4/handlers/handleEncryption.js
@@ -6,26 +6,31 @@ import {UPLOAD_DIR} from "../config.js";
 import {getFilePath} from "../util/filesystem.js";
 import {pipeline} from 'node:stream/promises'
 import fs from "node:fs";
+import {sendError} from "../util/error.js";
 
 export async function handleEncryption(req, res) {
     //Encryption algorithm
-    const algorithm = 'aes-256-cbc';
-    const asyncRandomBytes = promisify(crypto.randomBytes)
-    const key = await asyncRandomBytes(32);
-    const vector = await asyncRandomBytes(16);
-    const cipher = crypto.createCipheriv(algorithm,key,vector);
+    try{
+        const algorithm = 'aes-256-cbc';
+        const asyncRandomBytes = promisify(crypto.randomBytes)
+        const key = await asyncRandomBytes(32);
+        const vector = await asyncRandomBytes(16);
+        const cipher = crypto.createCipheriv(algorithm,key,vector);
 
-    const fileName = extractFileName(req.url,UPLOAD_DIR + '/encryption');
-    const filePath = getFilePath(fileName,UPLOAD_DIR)
-    const fileInputStream = fs.createReadStream(filePath);
-    const encryptFileName = fileName + '.enc';
-    const encryptFilePath = getFilePath(encryptFileName,UPLOAD_DIR);
-    const encryptOutputStream = fs.createWriteStream(encryptFilePath);
-    await pipeline(fileInputStream,cipher,encryptOutputStream);
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.end(JSON.stringify(({
-        success: true,
-        key: key.toString('hex'),
-        iv: vector.toString('hex')
-    })));
+        const fileName = extractFileName(req.url,UPLOAD_DIR + '/encryption');
+        const filePath = getFilePath(fileName,UPLOAD_DIR)
+        const fileInputStream = fs.createReadStream(filePath);
+        const encryptFileName = fileName + '.enc';
+        const encryptFilePath = getFilePath(encryptFileName,UPLOAD_DIR);
+        const encryptOutputStream = fs.createWriteStream(encryptFilePath);
+        await pipeline(fileInputStream,cipher,encryptOutputStream);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(({
+            success: true,
+            key: key.toString('hex'),
+            iv: vector.toString('hex')
+        })));
+    }catch (e){
+        sendError(res,404,e.message);
+    }
 }
