a85053dd5cd2b4d5e2da01ffd4d58e7706423086
erfan <erfan>
2025-11-25 18:41:02 +0330

    store full time and a uuid when uploading

--- HW4/source/handlers/uploadHandler.js
+++ HW4/source/handlers/uploadHandler.js
@@ -4,16 +4,19 @@ import {UPLOAD_DIR} from "../config.js";
 import {getFullPath} from "../util/filesystem.js";
 import {sendOk} from "../util/okResponse.js";
 import {sendError} from "../util/error.js";
+import {randomUUID} from 'node:crypto';
+
 export function handleUpload(req, res) {
-    const fileName = `${Date.now()}-uploaded`;
+    const uuid = randomUUID();
+    const date = new Date().toISOString()
+    const fileName = `${uuid}-${date}`;
     const filePath = getFullPath(path.join(UPLOAD_DIR,fileName));
     const fileWriteStream = fs.createWriteStream(filePath)
-
     fileWriteStream.on('error',(err) => {
         sendError(res,500,err.message);
     });
     req.pipe(fileWriteStream);
     req.on('end',() => {
-        sendOk(res,fileName,'file_uploaded');
+        sendOk(res,fileName,'file_uploaded',{id: uuid,date:date});
     });
 }
