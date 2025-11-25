81ff4c1274dbd32aa28b1aeb6a77049778ad1966
erfan <erfan>
2025-11-25 14:59:01 +0330

    change folder structure

--- HW4/source/util/filesystem.js
+++ HW4/source/util/filesystem.js
@@ -2,14 +2,14 @@ import fs from 'node:fs'
 import path from 'node:path'
 import {DOWNLOAD_DIR} from "../config.js";
 
-export function ensureDirExits(path) {
-    if(!fs.existsSync(path)){
-        fs.mkdirSync(path,{recursive : true});
-        console.log(`Created directory at: ${path}`);
+export function ensureDirExits(dirPath) {
+    if(!fs.existsSync(path.join('..',dirPath))){
+        fs.mkdirSync(path.join('..',dirPath),{recursive : true});
+        console.log(`Created directory at: ${dirPath}`);
     }
 }
 export function getFullPath(dirPath){
-    return path.join(process.cwd(),dirPath);
+    return path.join(process.cwd() ,'..',dirPath);
 }
 export function getFilePath(fileName, folderName = DOWNLOAD_DIR) {
     return getFullPath(path.join(folderName, fileName));
