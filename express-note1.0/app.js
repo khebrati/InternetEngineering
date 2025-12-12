fc3d2b1d810e1b106a669b6d7f01b0d6f3d7c10f
erfan <erfan>
2025-12-12 09:07:22 +0330

    add express js sample

---
+++ express-note1.0/app.js
@@ -0,0 +1,52 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const mongoose = require('mongoose');
+
+let indexRouter = require('./routes/index');
+let notesRouter = require('./routes/notes');
+
+
+const dbUrl = 'mongodb://127.0.0.1:27017/my_db1';
+const conn = mongoose.connect(dbUrl)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(function(req,res,next){
+//   console.log(req.cookies);
+//   next();
+// })
+app.use('/', indexRouter);
+app.use('/note', notesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
